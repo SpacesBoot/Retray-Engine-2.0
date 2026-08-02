efd3145a980113c4ee03dea0755a0f6d206e34d0
spacesxd <gutimili14@gmail.com>
2026-08-02 11:14:03 -0500

    movement upd

--- src/engine/3d/core3d.js
+++ src/engine/3d/core3d.js
@@ -32,6 +32,15 @@ function cam3d_third_person_fn(obj, offsett) {
   }
 }
 
+function displaceTo_fn(obj1, obj2, smoothness) {
+  let dirX = objectList[obj1].position.x - objectList[obj2].position.x;
+  let dirY = objectList[obj1].position.y - objectList[obj2].position.y;
+  let dirZ = objectList[obj1].position.z - objectList[obj2].position.z;
+
+  objectList[obj1].position.x += dirX * smoothness;
+  objectList[obj1].position.y += dirY * smoothness;
+  objectList[obj1].position.z += dirZ * smoothness;
+}
 
 function animate(){
   requestAnimationFrame(animate);
